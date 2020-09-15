1fda440ec308cb95b63a0b32395d3125f649f10f
Brian Taylor <brian.taylor@bolderflight.com>
2020-09-15 16:45:10 -0600

    Added tracking mode

--- include/control/pid.h
+++ include/control/pid.h
@@ -13,6 +13,7 @@ namespace controls {
 template<typename T>
 class Pid {
  public:
+  
   Pid(T kp, T min, T max) : kp_(kp), min_(min), max_(max) {}
   Pid(T kp, T ki, T dt, T min, T max) : kp_(kp), ki_(ki), dt_(dt), min_(min), max_(max) {}
   Pid(T kp, T ki, T kd, T tf, T dt, T min, T max) : kp_(kp), ki_(ki), kd_(kd), tf_(tf), dt_(dt), min_(min), max_(max) {}
@@ -37,10 +38,30 @@ class Pid {
     /* Return the command */
     return y_;
   }
-
+  T Run(T ref, T feedback, T tracking) {
+    /* Proportional error */
+    yp_ = kp_ * (b_ * ref - feedback);
+    /* Derivative error */
+    yd_ = (kd_ * (c_ * ref - feedback) - dstate_) * tf_;
+    /* Compute output */
+    y_ = yp_ + istate_ + yd_;
+    /* Saturation */
+    if ((y_ >= max_) && (ki_ * (ref - feedback) > 0))       {anti_windup_ = 0; y_ = max_;}
+    else if ((y_ >= max_) && (ki_ * (ref - feedback) <= 0)) {anti_windup_ = 1; y_ = max_;}
+    else if ((y_ <= min_) && (ki_ * (ref - feedback) >= 0)) {anti_windup_ = 1; y_ = min_;}
+    else if ((y_ <= min_) && (ki_ * (ref - feedback) < 0))  {anti_windup_ = 0; y_ = min_;}
+    else {anti_windup_ = 1;}
+    /* Derivative state */
+    dstate_ += dt_ * yd_;
+    /* Integrator state */
+    istate_ += anti_windup_ * dt_ * (ki_ * (ref - feedback) + kt_ * (tracking - y_));
+    /* Return the command */
+    return y_;
+  }
+  
  private:
   /* Control law constants */
-  const T kp_ = 0, ki_ = 0, kd_ = 0, tf_ = 0, b_ = 1, c_ = 1, dt_ = 0, min_, max_;
+  const T kp_ = 0, ki_ = 0, kd_ = 0, kt_ = 1, tf_ = 0, b_ = 1, c_ = 1, dt_ = 0, min_, max_;
   /* PID responses */
   T yp_ = 0, yd_ = 0;
   /* Derivative state */
